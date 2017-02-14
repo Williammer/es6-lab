51ccf09461406e92b9808705dd3d78a61e359084
Williammer <supervegeter@gmail.com>
2017-02-14 10:43:32 +0800

    Added fragmental template sample.

--- es6-shared.js
+++ es6-shared.js
@@ -1,1 +1,8 @@
 const print = input => console.log(input)
+
+const appendToBody = html => {
+	const body = document.body;
+	let wrap = document.createElement('div');
+	wrap.innerHTML = html;
+	body.appendChild(wrap);
+}
--- es6-template.js
+++ es6-template.js
@@ -1,21 +1,52 @@
-// basic template parse with es6 template literal tag function
+// 1. basic template parse with es6 template literal tag function
 function getFullString(strings, ...interpolatedValues) { // `...` essentially slices the arguments for us.
   return strings.reduce((total, current, index) => { // use an arrow function for brevity here
     total += current;
-    console.log('index: ' + index + ' | current: ' + current);
+    print('index: ' + index + ' | current: ' + current);
 
     if (interpolatedValues.hasOwnProperty(index)) {
       total += interpolatedValues[index];
-      console.log("interpolatedValues[index]: " + interpolatedValues[index]);
+      print("interpolatedValues[index]: " + interpolatedValues[index]);
     }
     return total;
   }, '');
 }
 
-
 const sb = 'SB',
+  sbn = 'n-SB',
   sbs = 'SBS';
-getFullString `basic template: Hello ${sb} and and ${sbs}`;
+getFullString `basic template: Hello
+${`nested template: ${sbn}`} and and ${sbs}`;
 
 
-// nested interpolated values
+/*
+ * 3. cleaner decoupled template fragments
+ */
+
+const capitalize = (string) => {
+  return string[0].toUpperCase() + string.slice(1).toUpperCase();
+};
+
+const deleteBtn = ({person, isAdmin}) => {
+    return isAdmin ? `<button>Delete ${capitalize(person)}</button>`: '';
+};
+
+const pplItem = ({person, isAdmin}) => {
+  return `<li>
+      <span>${capitalize(person)}</span>
+      ${deleteBtn({person, isAdmin})}
+    </li>`;
+};
+
+const pplList = ({people, isAdmin}) => {
+  return `<ul>
+    ${people.map(person => `${pplItem({person, isAdmin})}`)}
+    </ul>`;
+};
+
+const page3 = ({ people, isAdmin }) => {
+  return `<h2>People</h2>
+    ${pplList({ people, isAdmin })}`;
+};
+
+appendToBody(page3({ people: ['Keith', 'Dave', 'Amy' ], isAdmin: true }));
